2d6debf75044fbee0852a39f5823a89d5eea6f04
praveenasiva05 <praveenasiva055@gmail.com>
2025-10-13 00:04:16 +0530

    login update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
